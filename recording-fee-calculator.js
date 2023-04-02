39401cc293ee5189556ad542f6ee4d6f692d35ee
Nicole Groff <nlpgroff@gmail.com>
2023-04-02 07:12:24 -0400

    Revise constant variable names

--- recording-fee-calculator.js
+++ recording-fee-calculator.js
@@ -1,34 +1,34 @@
-const DEED_BASE_FEE = 70.25;
-const POA_BASE_FEE = 18.50;
-const MISC_BASE_FEE = 68.50;
-const SIMPLIFILE_FEE = 4.75;
-const BASE_PAGE_THRESHOLD = 4;
-const OVER_FOUR_PAGE_FEE = 2.00;
+const deedBaseFee = 70.25;
+const poaBaseFee = 18.50;
+const miscBaseFee = 68.50;
+const simplifileFee = 4.75;
+const basePageThreshold = 4;
+const overFourFeePerPage = 2.00;
 
 const getBaseFee = docType => {
     if (docType === "Deed") {
-        return DEED_BASE_FEE;
+        return deedBaseFee;
     } else if (docType === "Power of Attorney") {
-        return POA_BASE_FEE;
-    } else return MISC_BASE_FEE;
+        return poaBaseFee;
+    } else return miscBaseFee;
 }
 
 // Function which determines the total fee based on base fee and page count
 const getAdditionalPageFees = pageCount => {
     if (isNaN(pageCount) || pageCount < 1) {
         // throw an exception for invalid page count
-    } else if (pageCount <= BASE_PAGE_THRESHOLD) {
+    } else if (pageCount <= basePageThreshold) {
         return 0;
-    } else return (pageCount - BASE_PAGE_THRESHOLD) * OVER_FOUR_PAGE_FEE;
+    } else return (pageCount - basePageThreshold) * overFourFeePerPage;
 }
 
 // Function which asks whether "fee" is truthy; if so, 4.75 e-recording fee is returned
-const getERecordingFee = hasFee => hasFee ? SIMPLIFILE_FEE : 0;
+const getERecordingFee = hasFee => hasFee ? simplifileFee : 0;
 
 
-calculateTotalCharges ((docType, pageCount, hasFee) => {
-  const baseFee = getBaseFee(docType);
-  const additionalPageFees = getAdditionalPageFees(pageCount);
-  const eRecordingFee = getERecordingFee(hasFee);
-  return baseFee + additionalPageFees + eRecordingFee;
-})
+calculateTotalCharges((docType, pageCount, hasFee) => {
+    const baseFee = getBaseFee(docType);
+    const additionalPageFees = getAdditionalPageFees(pageCount);
+    const eRecordingFee = getERecordingFee(hasFee);
+    return baseFee + additionalPageFees + eRecordingFee;
+});
