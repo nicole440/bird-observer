eee69ec78aee3dadcf87135b04a30f664bf7c5e3
Nicole Groff <nlpgroff@gmail.com>
2023-04-14 21:00:44 -0400

    Clean up methods, add classes, and update styling

--- recording-fee-calculator/src/App.vue
+++ recording-fee-calculator/src/App.vue
@@ -1,102 +1,46 @@
 <template>
   <div id="app">
-    <h1>Recording Fee Calculator</h1>
-    <hello-world/>
-    <input-form/>
+      <div class="container">
+        <InputForm />
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import InputForm from './components/InputForm.vue'
 
 export default {
   name: 'App',
   components: {
     InputForm,
-    HelloWorld
   }
 }
 </script>
 
-// TODO update styles to reflect revised elements
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@300&family=Red+Hat+Mono:wght@600&display=swap");
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: left;
   color: #2c3e50;
-  margin-top: 60px;
-}
-/*
-@import url("https://fonts.googleapis.com/css2?family=Indie+Flower&family=Raleway:wght@300&family=Red+Hat+Mono:wght@600&display=swap");
-
-body {
-  background-image: url("https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80");
+  background: #eee;
+  background-image: url(https://scontent.fwbw1-1.fna.fbcdn.net/v/t1.6435-9/89690658_10157175219921680_3024916127651201024_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=MgTQdfH_teIAX-tXofz&_nc_ht=scontent.fwbw1-1.fna&oh=00_AfC_dNoTA01myjff1ZqAAOKWxXJNcHNJ58ixxFsvdWXHHw&oe=646166F7);
+  /* background-image: url(https://images.unsplash.com/photo-1593018875302-1867d4de942c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80); */
   background-repeat: no-repeat;
+  background-position: center;
   background-size: cover;
-  background-attachment: fixed;
-  display: grid;
+  min-height: 100vh;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+.container {
   margin: 0 auto;
+  max-width: 960px;
+  padding: 1rem;
 }
 * {
   box-sizing: border-box;
-  text-align: center;
 }
-a {
-  text-decoration: none;
-  color: #00addf;
-}
-a:hover {
-  color: #000;
-}
-h1,
-h2,
-h3,
-h4,
-p,
-a,
-label {
-  font-family: "Raleway", sans-serif;
-}
-h1 {
-  font-weight: bold;
-  font-size: 3em;
-}
-span {
-  font-family: "Indie Flower", cursive;
-  font-size: 1.5em;
-  font-weight: bold;
-}
-
-/* Media queries no longer needed with Vue framework
-@media only screen and (max-width: 480px) {
-  .header-container,
-  footer {
-    width: 100%;
-    min-height: 200px;
-    overflow: visible;
-  }
-  .header-img {
-    max-height: 100%;
-  }
-  .header-text {
-    min-width: 450px;
-  }
-  h1 {
-    font-size: 2em;
-  }
-  h2,
-  h3,
-  h4,
-  p,
-  a {
-    font-size: 1.5em;
-  }
-  .about-text {
-    min-width: 85%;
-  }
-}
-*/
 </style>
--- recording-fee-calculator/src/components/InputForm.vue
+++ recording-fee-calculator/src/components/InputForm.vue
@@ -1,103 +1,217 @@
 <template>
-  <div class="form">
-    <h1>Hello, World</h1>
-    <form>
-      <div>
-        <label for="document-type">Document Type:</label>
-        <select id="document-type" v-model="documentType">
-          <option value="Deed">Deed</option>
-          <option value="Mortgage">Mortgage</option>
-          <option value="Power of Attorney">Power of Attorney</option>
-          <option value="Assignment of Rents/Leases">
-            Assignment of Rents/Leases
-          </option>
-          <option value="Satisfaction Piece">Satisfaction Piece</option>
-          <option value="Miscellaneous">Miscellaneous</option>
-        </select>
-      </div>
-      <div>
-        <label for="page-count">Page Count:</label>
-        <input
-          id="page-count"
-          type="number"
-          v-model.number="pageCount"
-          min="1"
-        />
-      </div>
-      <div>
-        <label>
-          <input type="checkbox" v-model="simplifileFee" />
-          Add Simplifile Fee ($4.75)
-        </label>
-      </div>
-      <button type="button" @click="calculateFee">Calculate Fee</button>
-    </form>
-    <p>Total Charges: ${{ calculateTotalCharges.toFixed(2) }}</p>
+  <div class="container">
+    <div class="form">
+      <h1 class="form-title">Recording Fee Calculator</h1>
+      <form class="form-body">
+        <div class="form-group">
+          <label for="document-type" class="form-label">Document Type:</label>
+          <select id="document-type" v-model="documentType" class="form-select">
+            <option value="Deed">Deed</option>
+            <option value="Mortgage">Mortgage</option>
+            <option value="Power of Attorney">Power of Attorney</option>
+            <option value="Assignment of Rents/Leases">
+              Assignment of Rents/Leases
+            </option>
+            <option value="Satisfaction Piece">Satisfaction Piece</option>
+            <option value="Miscellaneous">Miscellaneous</option>
+          </select>
+        </div>
+        <div class="form-group">
+          <label for="page-count" class="form-label">Page Count:</label>
+          <input
+            id="page-count"
+            type="number"
+            v-model.number="pageCount"
+            min="1"
+            class="form-input"
+          />
+        </div>
+        <div class="form-group">
+          <label v-if="this.document == 'Deed'">
+            <input type="radio" v-model="hasTransferTax" v-bind:value="0">Exempt
+            <input type="radio" v-model="hasTransferTax" v-bind:value="1">Not Exempt
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" v-model="hasFee" />
+            Add Simplifile Fee ($4.75)
+          </label>
+        </div>
+        <button type="button" @click="calculateTotalCharges" class="form-button">Calculate Fee</button>
+      </form>
+      <p class="form-total">Total Charges: ${{ calculateTotalCharges.toFixed(2) }}</p>
+    </div>
   </div>
 </template>
-
 <script>
 export default {
   name: "InputForm",
+
   data() {
     return {
-      totalfee: 0
+      DEED_BASE_FEE: 70.25,
+      POA_BASE_FEE: 18.5,
+      MISC_BASE_FEE: 68.5,
+      BASE_PAGE_THRESHOLD: 4,
+      SIMPLIFILE_FEE: 4.75,
+      OVER_FOUR_PAGE_FEE: 2.0,
+      documentType: "",
+      pageCount: 1,
+      hasFee: false,
+      hasTransferTax: false
     };
   },
   computed: {
-    getBaseFee(documentType) {
+    getBaseFee() {
       // Calculate base fee
-      const DEED_BASE_FEE = 70.25;
-      const POA_BASE_FEE = 18.5;
-      const MISC_BASE_FEE = 68.5;
       let baseFee = 0;
-      if (documentType === "Deed") {
-        baseFee = DEED_BASE_FEE;
-      } else if (documentType === "Power of Attorney") {
-        baseFee = POA_BASE_FEE;
+      if (this.documentType === "Deed") {
+        baseFee = this.DEED_BASE_FEE;
+      } else if (this.documentType === "Power of Attorney") {
+        baseFee = this.POA_BASE_FEE;
       } else {
-        baseFee = MISC_BASE_FEE;
+        baseFee = this.MISC_BASE_FEE;
       }
       return baseFee;
     },
-    getAdditionalPageFee(pageCount) {
+    getAdditionalPageFee() {
       // Calculate additional pages fee
-      const BASE_PAGE_THRESHOLD = 4;
-      const OVER_FOUR_PAGE_FEE = 2.0;
       let additionalPageFee = 0;
-      if (isNaN(pageCount) || pageCount < 1) {
+      if (isNaN(this.pageCount) || this.pageCount < 1) {
         throw new Error("Invalid page count");
-      } else if (pageCount <= BASE_PAGE_THRESHOLD) {
+      } else if (this.pageCount <= this.BASE_PAGE_THRESHOLD) {
         return 0;
       } else {
         additionalPageFee =
-          (pageCount - BASE_PAGE_THRESHOLD) * OVER_FOUR_PAGE_FEE;
+          (this.pageCount - this.BASE_PAGE_THRESHOLD) * this.OVER_FOUR_PAGE_FEE;
       }
       return additionalPageFee;
     },
-
-    getERecordingFee(hasFee) {
-      const SIMPLIFILE_FEE = 4.75;
-      if (hasFee) {
-        return SIMPLIFILE_FEE;
-      } else return 0;
+    calculateTransferTax() {
+      if (this.hasTransferTax == 0) {
+        return false;
+      } return true;
     },
-
+    getERecordingFee() {
+      if (this.hasFee) {
+        return this.SIMPLIFILE_FEE;
+      } else {
+        return 0;
+      }
+    },
     calculateTotalCharges() {
       let total = 0;
       total =
-        this.getBaseFee(this.documentType) +
-        this.getAdditionalPageFee(this.pageCount) +
-        this.getERecordingFee(this.hasFee);
+        this.getBaseFee +
+        this.getAdditionalPageFee +
+        this.getERecordingFee;
       return total;
     },
   },
 };
 </script>
-  
 <style scoped>
-#input-form {
-  grid-template-columns: repeat(3, 1fr);
-  grid-auto-rows: minmax(150px, auto);
+/* Container */
+.form-container {
+  max-width: 600px;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
+
+/* Form */
+.form {
+  width: 100%;
+  max-width: 500px;
+  background-color: #fff;
+  padding: 40px;
+  /* border-radius: 8px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); */
+  text-align: center;
+}
+
+/* Title */
+.form h1 {
+  margin-bottom: 30px;
+  font-size: 36px;
+  font-family: "Raleway", sans-serif;
+}
+
+/* Labels */
+.form label {
+  display: block;
+  margin-bottom: 8px;
+  font-weight: 500;
+}
+
+/* Select */
+.form select {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 16px;
+  font-size: 16px;
+  font-weight: 400;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background-repeat: no-repeat;
+  background-position: right 10px center;
+  background-size: 16px;
+  color: #333;
+}
+
+/* Input */
+.form input {
+  display: block;
+  width: 100%;
+  padding: 10px;
+  margin-bottom: 16px;
+  font-size: 16px;
+  font-weight: 400;
+  border-radius: 4px;
+  border: 1px solid #ddd;
+  color: #333;
+}
+
+/* Checkbox */
+.form input[type="checkbox"] {
+  display: inline-block;
+  margin-right: 8px;
+  margin-bottom: 0;
+  font-size: 16px;
+  font-weight: 400;
+  color: #333;
+}
+
+/* Button */
+.form button {
+  display: block;
+  width: 100%;
+  padding: 12px;
+  margin-top: 16px;
+  background-color: #333;
+  color: #fff;
+  font-size: 16px;
+  font-weight: 500;
+  cursor: pointer;
+  transition: background-color 0.2s ease;
+}
+
+.form button:hover {
+  background-color: #555;
+}
+
+/* Result */
+.form p {
+  margin-top: 32px;
+  font-size: 24px;
+  font-weight: 500;
+}
+
 </style>
